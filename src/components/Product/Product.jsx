353a8fc5134ea86db82f479a17a8c1ceb2994289
Anna Baranova <asbaranova1@gmail.com>
2021-08-04 22:56:53 -0400

    add toggle for description

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,52 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "../Rating/Rating";
 import "./Product.css";
 
-const Product = ({ product }) => {
 
+const Product = ({ product }) => {
+  const [show, setShow] = useState(false);
+  const shortDescription = (""+product.description).slice(0, 50)
 
   return (
     <div key={product._id} className="card mb-3 card-size">
       <Link to={`/products/${product._id}`} className="card-header">
         <h1>{product.name}</h1>
       </Link>
-      <img
-          className="card-img-top"
-          src={product.img}
-          alt={product.name}
-        ></img>
-      {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="d-block user-select-none"
-          width="100%"
-          height="200"
-          aria-label="Placeholder: Image cap"
-          focusable="false"
-          role="img"
-          preserveAspectRatio="xMidYMid slice"
-          viewBox="0 0 318 180"
-          //   style="font-size:1.125rem;text-anchor:middle"
-        >
-          <img src={product.img} alt={product.name}></img>
-        </svg> */}
+      <img className="card-img-top" src={product.img} alt={product.name}></img>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">{product.description}</li>
+        <li className="list-group-item">
+          {show ? (
+            <>
+              <button
+                className="btn btn-default"
+                onClick={() => setShow(false)}
+              >
+                Show less
+              </button>
+              <div>
+              {product.description}
+              </div>
+            </>
+          ) : (
+            <>
+              <button className="btn btn-default" onClick={() => setShow(true)}>
+                Show more
+              </button>
+              <div>
+                {shortDescription}
+              </div>
+            </>
+          )}
+        </li>
         <li className="list-group-item">{product.price} CAD</li>
       </ul>
       <Rating
         value={product.avRating}
-        text={`${product.reviews.length > 0 ? product.reviews.length : 0} reviews`}
+        text={`${
+          product.reviews.length > 0 ? product.reviews.length : 0
+        } reviews`}
         color="#f8e825"
       />
-      {/* <div className="card-body">
-          <a href="#" className="card-link">
-            Card link
-          </a>
-          <a href="#" className="card-link">
-            Another link
-          </a>
-        </div> */}
       <div className="card-footer text-muted">
         {product.quantity > 0 ? "In stock" : "Out of Stock"}
       </div>
